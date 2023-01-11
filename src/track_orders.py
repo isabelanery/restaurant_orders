45c5574d22b7803df387dab166d5b2f40244880e
Isabela Nery <isabelanery.r@gmail.com>
2023-01-11 19:48:11 -0300

    feat: track_orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,62 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.__orders = []
+
     def __len__(self):
-        pass
+        return len(self.__orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        new_order = {"customer": customer, "order": order, "day": day}
+        self.__orders.append(new_order)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return Counter(
+            [
+                order.get("order")
+                for order in self.__orders
+                if order.get("customer") == customer
+            ]
+        ).most_common(1)[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        client_orders = [
+            order
+            for order in self.__orders
+            if order.get("customer") == customer
+        ]
+
+        all_dishes = {order.get("order") for order in self.__orders}
+        client_dishes = {order.get("order") for order in client_orders}
+
+        return all_dishes - client_dishes
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        client_orders = [
+            order
+            for order in self.__orders
+            if order.get("customer") == customer
+        ]
+
+        all_days = {order.get("day") for order in self.__orders}
+        client_days = {order.get("day") for order in client_orders}
+
+        return all_days - client_days
 
     def get_busiest_day(self):
-        pass
+        return Counter(
+            [
+                order.get("day")
+                for order in self.__orders
+            ]
+        ).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        return Counter(
+            [
+                order.get("day")
+                for order in self.__orders
+            ]
+        ).most_common()[-1][0]
